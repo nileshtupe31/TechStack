a2643035e65a34a4f8a85efb1277c3a869d40cbc
Nilesh Tupe <nilesh.tupe@fusionops.com>
2018-04-30 20:26:25 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Header } from './src/components/common';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Header> Tech Stack </Header>
+      </View>
+    );
+  }
+}
