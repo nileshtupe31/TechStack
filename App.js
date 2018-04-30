9e5f3c2d55c4fb2041ce6e274f50848f83da1e16
Nilesh Tupe <nilesh.tupe@fusionops.com>
2018-04-30 20:58:23 +0530

    Added redux library

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import { Header } from './src/components/common';
+import { reducers } from './src/reducers';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <Header> Tech Stack </Header>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Header> Tech Stack </Header>
+        </View>
+      </Provider>
     );
   }
 }
